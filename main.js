d41becde008dd9aed992fd49e93cce28043ab767
Nate West <southbynathanwest@yahoo.com>
2023-03-02 14:55:08 -0600

    initial commit maybe

---
+++ main.js
@@ -0,0 +1,9 @@
+const _ = require('lodash')
+
+// Use fake data or APIs you have worked with in previous projects or google fake data you can use. - [ ] Either make a fetch or save it in a few variables.
+// Console.log the output of each lodash method called on the fake data.
+// Use at least these methods: _.chunk(), _.reverse(), _.without(), _.shuffle().
+// Create unit tests with Mocha for each function you build.
+
+const numbers = [8, 16, 24, 32, 40, 48, 56, 64, 72];
+
